62fa4c7cfdd0f0b8e3cc73b8d39b96cb2a918970
Alvaro <buntupana@gmail.com>
2020-02-28 00:57:29 +0100

    add libraries and create application class

---
+++ app/src/main/java/com/buntupana/qbittorrentremote/App.kt
@@ -0,0 +1,16 @@
+package com.buntupana.qbittorrentremote
+
+import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+
+        AndroidThreeTen.init(this)
+    }
+}
